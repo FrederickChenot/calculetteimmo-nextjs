16858397947396a3fdb14e35389f9807b7fab7fd
Fred'Dev <fredchenot@gmail.com>
2026-04-18 23:16:06 +0200

    redirect non-www vers www

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "calculetteimmo.com" }],
+        destination: "https://www.calculetteimmo.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
